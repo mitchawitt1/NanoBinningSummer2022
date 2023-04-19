9abdb4480da6cc6aed6294ad2d771f463a657789
Mitchell Witt <124813992+mitchawitt1@users.noreply.github.com>
2023-04-19 14:24:54 -0400

    Add files via upload

--- snakemake_binning_assessment/run_snakemake.sh
+++ snakemake_binning_assessment/run_snakemake.sh
@@ -3,11 +3,11 @@
 #SBATCH --partition=panda   # cluster-specific
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=snakemake_binning_assessment
+#SBATCH --job-name=sm_read_simulation
 #SBATCH --time=16:00:00   # HH/MM/SS
-#SBATCH --mem=28G   # memory requested, units available: K,M,G,T
-#SBATCH --output sm_autobin.out
-#SBATCH --error sm_autobin.err
-source ~/.bashrc
+#SBATCH --mem=15G   # memory requested, units available: K,M,G,T
+#SBATCH --output sm_readsim.out
+#SBATCH --error sm_readsim.err
 
-snakemake --cores 8
+source ~/.bashrc
+snakemake --cores 1
