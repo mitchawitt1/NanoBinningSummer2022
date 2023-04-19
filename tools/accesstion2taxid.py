fc6c679aa8b2706c7b5fa10175395d1114eaed59
Mitchell Witt <124813992+mitchawitt1@users.noreply.github.com>
2023-04-19 14:42:00 -0400

    Update accesstion2taxid.py

--- tools/accesstion2taxid.py
+++ tools/accesstion2taxid.py
@@ -6,15 +6,3 @@ with gzip.open('nucl_gb.accession2taxid.gz') as fh:
     for line in fh:
         line = line.split()
         dic[line[0]] = line[1]
-
-print(dic['A00001'])
-# * 
-#blast = 'A00001.1'
-#for line in blast:
-#    line = line.split()
-#    if line[1] in dic:
-#        print('%s\t%s\t%s'%(line[0],line[1],dic[line[1]]))
-#    else:
-#        print('%s\t%s\tNA'%(line[0],line[1]))
-
-
