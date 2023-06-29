2f5cec840c65a81b5dcafe9ca70714d6b949a432
Mitchell Witt <124813992+mitchawitt1@users.noreply.github.com>
2023-06-29 11:51:44 -0400

    Add files via upload

--- snakemake_read_simulation/run_snakemake.sh
+++ snakemake_read_simulation/run_snakemake.sh
@@ -6,8 +6,8 @@
 #SBATCH --job-name=sm_read_simulation
 #SBATCH --time=16:00:00   # HH/MM/SS
 #SBATCH --mem=15G   # memory requested, units available: K,M,G,T
-#SBATCH --output sm_readsim.out
-#SBATCH --error sm_readsim.err
+#SBATCH --output new_10S10C1_readsim.out
+#SBATCH --error new_10S10C1_readsim.err
 
 source ~/.bashrc
 snakemake --cores 1
